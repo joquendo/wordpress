4492eb04f0754db6972c3998a00fa8e5f4cc230c
James Oquendo <james.oquendo@gmail.com>
2015-03-18 10:40:35 -0700

    Creating a child theme of twentytwelve

---
+++ blueprint/wp-content/themes/twentytwelve-blueprint/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+// Setup a child theme--http://codex.wordpress.org/Child_Themes
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function theme_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array('parent-style')
+	);
+}
