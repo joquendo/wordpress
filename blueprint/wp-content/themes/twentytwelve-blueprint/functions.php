18c09602a13f5fd385e629447d19cd489e777cdf
James Oquendo <james.oquendo@gmail.com>
2015-03-18 18:09:38 -0700

    Displaying custom post types on the front page

--- blueprint/wp-content/themes/twentytwelve-blueprint/functions.php
+++ blueprint/wp-content/themes/twentytwelve-blueprint/functions.php
@@ -84,3 +84,12 @@ function post_type_updated_messages($messages) {
 	return $messages;
 
 }
+
+// Displaying custom post types on the front page
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+
+function add_my_post_types_to_query( $query ) {
+	if ( is_home() && $query->is_main_query() )
+		$query->set( 'post_type', array( 'post', 'book' ) );
+	return $query;
+}
