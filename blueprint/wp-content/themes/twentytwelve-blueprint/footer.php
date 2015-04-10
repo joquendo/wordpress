f4ebd8b1fefcceddaf82a4ffcff779cc213111e7
modernrockstar <joe@modernrockstar.com>
2015-04-10 15:11:52 -0700

    Footer styling

--- blueprint/wp-content/themes/twentytwelve-blueprint/footer.php
+++ blueprint/wp-content/themes/twentytwelve-blueprint/footer.php
@@ -13,31 +13,48 @@ $about_us_id = get_id_by_slug('about'); //RETRIEVES ID OF PAGE/POST WITH SLUG - 
 ?>
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
-		<div class="column">
-			<h3>About Us</h3>
-			<p><?php echo wp_trim_words(get_post_field('post_content', $about_us_id), 20, '...');?> <a href="/about">MORE</a></p>
-		</div>
-		<div class="column">
-		</div>
-		<div class="column">
-		</div>
-			<h3>Contact Us</h3>
-			<p>
-				<?php echo get_field('street_address', $about_us_id); ?><br/>
-				<?php echo get_field('city', $about_us_id); ?>, <?php echo get_field('state', $about_us_id); ?> <?php echo get_field('zip', $about_us_id); ?>
-			</p>
-			<p>
-				P: <?php echo get_field('phone', $about_us_id); ?><br/>
-				E: <a href="mailto:<?php echo get_field('email', $about_us_id); ?>"><?php echo get_field('email', $about_us_id); ?></a>
-			</p>
-			<p><a href="https://twitter.com/<?php echo get_field('twitter_username', $about_us_id); ?>">Twitter</a></a>
-
-			<h3>Stay Informed</h3>
-			<?php echo do_shortcode('[contact-form-7 id="194" title="Stay Informed"]');?>
+		<div class="siteinfo">
+			<div class="column first">
+				<div class="content">
+					<h6>About Us</h6>
+					<p><?php echo wp_trim_words(get_post_field('post_content', $about_us_id), 20, '...');?> <a href="/about">MORE</a></p>
+				</div>
+				<div class="content">
+					<p><a href="/staff-and-contributors">Staff and Contributors</a></p>
+				</div>
+			</div>
+			
+			<div class="column middle">
+				<div class="content">
+					<h6>Partners</h6>
+					<?php wp_nav_menu( array('menu' => 'Partners' )); ?>
+				</div>
+				<div class="content">
+					<h6>Contact Us</h6>
+					<p>
+						<?php echo get_field('street_address', $about_us_id); ?><br/>
+						<?php echo get_field('city', $about_us_id); ?>, <?php echo get_field('state', $about_us_id); ?> <?php echo get_field('zip', $about_us_id); ?>
+					</p>
+					<p>
+						P: <?php echo get_field('phone', $about_us_id); ?><br/>
+						E: <a href="mailto:<?php echo get_field('email', $about_us_id); ?>"><?php echo get_field('email', $about_us_id); ?></a>
+					</p>
+				</div>
+			</div>
+			
+			<div class="column last">
+				<div class="content">
+					<h6>Stay Informed</h6>
+					<?php echo do_shortcode('[contact-form-7 id="194" title="Stay Informed"]');?>
+				</div>
+				<div class="content">
+					<p><a href="https://twitter.com/<?php echo get_field('twitter_username', $about_us_id); ?>">Twitter</a></a>
+				</div>
+			</div>
 		</div><!-- .site-info -->
 
 		<div class="legal">
-			&copy;2015 UC Regents, All Rights Reserved
+			&copy;<?php echo date('Y');?> UC Regents, All Rights Reserved
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
