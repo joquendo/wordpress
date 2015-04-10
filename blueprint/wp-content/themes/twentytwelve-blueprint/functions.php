25feca7a8a2221f57b8fdbfe0e48f1955c5d7ea4
modernrockstar <joe@modernrockstar.com>
2015-04-10 15:13:38 -0700

    Registered footer.css to header in function file

--- blueprint/wp-content/themes/twentytwelve-blueprint/functions.php
+++ blueprint/wp-content/themes/twentytwelve-blueprint/functions.php
@@ -9,6 +9,7 @@ function theme_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style',  get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
 	wp_enqueue_style( 'sidebar-style',  get_stylesheet_directory_uri() . '/css/sidebar.css', array('parent-style') );
+	wp_enqueue_style( 'footer-style',  get_stylesheet_directory_uri() . '/css/footer.css', array('parent-style') );
 }
 
 // Remove custom font enabled in twentytwelve theme
