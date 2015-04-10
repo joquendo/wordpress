0013dfa932443cea25dfe2695eda8f29f1f54c7b
modernrockstar <joe@modernrockstar.com>
2015-04-10 13:54:21 -0700

    Added comment about function get_id for footer.php

--- blueprint/wp-content/themes/twentytwelve-blueprint/footer.php
+++ blueprint/wp-content/themes/twentytwelve-blueprint/footer.php
@@ -9,13 +9,18 @@
  * @since Twenty Twelve 1.0
  */
 
-$about_us_id = get_id_by_slug('about');
+$about_us_id = get_id_by_slug('about'); //RETRIEVES ID OF PAGE/POST WITH SLUG - FUNCTIONS.PHP
 ?>
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
-		<div class="site-info">
+		<div class="column">
 			<h3>About Us</h3>
 			<p><?php echo wp_trim_words(get_post_field('post_content', $about_us_id), 20, '...');?> <a href="/about">MORE</a></p>
+		</div>
+		<div class="column">
+		</div>
+		<div class="column">
+		</div>
 			<h3>Contact Us</h3>
 			<p>
 				<?php echo get_field('street_address', $about_us_id); ?><br/>
@@ -30,6 +35,10 @@ $about_us_id = get_id_by_slug('about');
 			<h3>Stay Informed</h3>
 			<?php echo do_shortcode('[contact-form-7 id="194" title="Stay Informed"]');?>
 		</div><!-- .site-info -->
+
+		<div class="legal">
+			&copy;2015 UC Regents, All Rights Reserved
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
