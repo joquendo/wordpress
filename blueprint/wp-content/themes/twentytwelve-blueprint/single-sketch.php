a9cb548688cc1934fd45b35dffd622a911da0223
modernrockstar <joe@modernrockstar.com>
2015-04-15 12:34:50 -0700

    Added single sketch type

---
+++ blueprint/wp-content/themes/twentytwelve-blueprint/single-sketch.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', get_post_format() ); ?>
+
+				<nav class="nav-single">
+					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
+					<?php
+						$post_object = get_field('related_article');
+
+						if( $post_object ): 
+							// override $post
+							$post = $post_object;
+							setup_postdata( $post ); 
+					?>
+					<div id="related-article">
+						<h2 class="post-type">Related</h2>
+						<?php if ( get_field('thumbnail') ) : ?>
+							<div class="entry-image"><a href="<?php the_permalink(); ?>" rel="bookmark"><img src="<?php the_field('thumbnail'); ?>" alt="" /></a></div>
+						<?php elseif ( 'sketch' == get_post_type() ) : ?>
+							<div class="sketch-image"><a href="<?php the_permalink(); ?>" rel="bookmark"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-editors-note.png" alt="" /></a></div>
+						<?php endif; ?>
+						<div class="entry-summary <?php ( get_field('thumbnail') ) ? print 'has-image' : print '' ?> <?php ( 'sketch' == get_post_type() ) ? print 'has-sketch-image' : print '' ?>">
+							<?php if ( get_field('article_type') ) : ?>
+								<span class="article-type"><?php echo get_field('article_type'); ?></span>
+							<?php endif; ?>	
+							<h2 class="entry-title">
+								<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+							</h2>
+							<?php the_excerpt(); ?>
+							<p class="entry-meta"><?php twentytwelve_entry_meta(); ?></p>
+						</div>
+					</div>
+						<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+						<?php endif; ?>
+				</nav><!-- .nav-single -->
+
+				<?php comments_template( '', true ); ?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
