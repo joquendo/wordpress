e51b4051fb528c9154424f3f46a5a14f01eb304a
John Kim <johnkim@support.ucla.edu>
2015-04-27 08:51:51 -0700

    Header Fix

--- blueprint/wp-content/themes/twentytwelve-blueprint/header.php
+++ blueprint/wp-content/themes/twentytwelve-blueprint/header.php
@@ -38,6 +38,7 @@ if($post_type === 'issue') $issueID = get_the_ID();
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
+
 </head>
 
 <body <?php body_class(); ?>>
