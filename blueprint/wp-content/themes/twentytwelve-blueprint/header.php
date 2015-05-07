6c15847d96c626391da91b9b6af2abd287ab8438
modernrockstar <joe@modernrockstar.com>
2015-05-07 16:07:21 -0700

    Added Favicon

--- blueprint/wp-content/themes/twentytwelve-blueprint/header.php
+++ blueprint/wp-content/themes/twentytwelve-blueprint/header.php
@@ -33,6 +33,7 @@ if($post_type === 'issue') $issueID = get_the_ID();
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri();?>/images/favicon.ico">
 <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
